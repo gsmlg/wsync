3ef684366c935284fb1449797188a94617f9bd5d
gaoshiming <gaoshiming@zdns.cn>
2017-06-29 19:17:32 +0800

    udpate test

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['mocha', 'coverage'],
 
 
     // web server port
@@ -93,6 +93,13 @@ module.exports = function(config) {
         ]
       },
     },
+
+    coverageReporter: {
+      reporters: [
+        { type: 'text' },
+        { type: 'text-summary' },
+      ]
+    }
 
   });
 
